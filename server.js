ff5989985ab84f54ba3eb90b459f565605ae748b
Arthur <arthurtatarin@gmail.com>
2020-02-07 10:17:23 +0200

    add

--- server.js
+++ server.js
@@ -14,5 +14,3 @@ res.sendFile(path.join(__dirname,'dist/CarRentalAngular/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log('server.js');
